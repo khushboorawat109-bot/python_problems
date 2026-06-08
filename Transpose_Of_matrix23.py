78c1ec9f290e59126273a1a2704bc85a75b74536
Khushboo Rawat <khushboorawat109@gmail.com>
2026-06-08 15:27:46 +0530

    Transpose of a matrix

---
+++ Transpose_Of_matrix23.py
@@ -0,0 +1,11 @@
+matrix = [[1, 2], [3, 4]]
+
+transpose = []
+
+for i in range(len(matrix[0])):
+    row = []
+    for j in range(len(matrix)):
+        row.append(matrix[j][i])
+    transpose.append(row)
+
+print(transpose)

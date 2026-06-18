d7f45284c767572a283a6107884795036644b7ea
Khushboo Rawat <khushboorawat109@gmail.com>
2026-06-18 12:12:41 +0530

    bubble sort logic

---
+++ bubble.py
@@ -0,0 +1,8 @@
+arr = [5, 3, 8, 4, 2]
+
+for i in range(len(arr)):
+    for j in range(0, len(arr) - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print(arr)
